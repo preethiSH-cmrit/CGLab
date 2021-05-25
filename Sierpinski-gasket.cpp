390e20898a139fea221904fcefa299d4ae8b5b40
preethiSH-cmrit <54387257+preethiSH-cmrit@users.noreply.github.com>
2021-05-25 12:10:26 +0530

    Rename main.cpp to Sierpinski-gasket.cpp

--- Sierpinski-gasket.cpp
+++ Sierpinski-gasket.cpp
@@ -102,7 +102,7 @@ int main(int argc, char **argv){
     glutInit(&argc,argv);
     glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
     glutInitWindowSize(500,500);
-    glutCreateWindow("Sierpinski Gasket-2D using points");
+    glutCreateWindow("SierpinskiÂ Gasket-2D using points");
     myinit();
     glutDisplayFunc(display);
     glutMainLoop();
